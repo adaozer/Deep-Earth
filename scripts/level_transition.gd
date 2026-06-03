b32b4efa4a81b8b583d691e57ea254a87ed667fc
adaozer <115459409+adaozer@users.noreply.github.com>
2026-06-03 11:24:11 +0100

    edited level 2

--- scripts/level_transition.gd
+++ scripts/level_transition.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
-		get_tree().change_scene_to_file.call_deferred('./scenes/' + next_scene)
+		get_tree().change_scene_to_file.call_deferred('./scenes/' + next_scene + '.tscn')
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
